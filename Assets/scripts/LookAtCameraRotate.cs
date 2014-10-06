19259b1c57084a9283cf905aa2384b8e7a347a89
Stewart Martin <stewartjmartin@gmail.com>
2014-10-05 23:47:03 -0500

    Added flying voxel pigs from "concept art challenge" to login, because I wanted to. :D

---
+++ Assets/scripts/LookAtCameraRotate.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookAtCameraRotate : MonoBehaviour {
+
+		public GameObject target;//the target object
+		private float speedMod = 4.0f;//a speed modifier
+		private Vector3 point;//the coord to the point where the camera looks at
+		
+		void Start () {//Set up things on the start method
+			point = target.transform.position;//get target's coords
+			transform.LookAt(point);//makes the camera look to it
+		}
+		
+		void Update () {//makes the camera rotate around "point" coords, rotating around its Y axis, 20 degrees per second times the speed modifier
+			transform.RotateAround (point,new Vector3(0.0f,1.0f,0.0f),20 * Time.deltaTime * speedMod);
+		}
+	}
